17ef359e568b0aae83f9787a7ec1a5fd841455b0
Ian Pan <ianpan@hotmail.com>
2017-02-23 23:13:32 +0000

    reverseString logic and refactored

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
+  var array = [];
+//   array = str.split('');
+//   array = array.reverse();
+//   str = array.join('');
   
+  str = str.split('').reverse().join('');
   
   return str;
 }
