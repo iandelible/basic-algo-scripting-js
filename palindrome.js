1bd2cd3bdf90ce832b3c865fe8f40829554ec3c1
Ian Pan <ianpan@hotmail.com>
2017-02-25 21:49:54 +0000

    add RegExp for explanation

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,5 @@
 function palindrome(str) {
-  //originally used a /\W/g for taking out non-alphanumerics, but the _(underscore) doesn't get taken out
+  //originally used a RegExp /\W/g for taking out non-alphanumerics, but the _(underscore) doesn't get taken out
   var forward = str.replace(/[^a-z0-9z]/gi, '').toLowerCase().split('').join(''); 
   var backward = str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('').reverse().join('');
   if (forward === backward) {
