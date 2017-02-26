ee0969e35f936603f7e84388cc7da11578ab36f7
Ian Pan <ianpan@hotmail.com>
2017-02-26 22:21:17 +0000

    first solution titleCase

--- titlecase.js
+++ titlecase.js
@@ -1,6 +1,11 @@
 function titleCase(str) {
+  var array = str.toLowerCase().split(" ");
+  var titleCase = array.map(function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  });
+  var last = titleCase.join(" ");
   
-  return str;
+  return last;
 }
 
 titleCase("I'm a little tea pot");
