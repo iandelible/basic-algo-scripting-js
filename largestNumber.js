9d77641b1b64a0fad17bb5c791317b74b9d9d4d6
Ian Pan <ianpan@hotmail.com>
2017-02-27 23:03:15 +0000

    working solution largestOfFour

--- largestNumber.js
+++ largestNumber.js
@@ -3,12 +3,20 @@ function largestOfFour(arr) {
   // loop through each element of the initial array
   // sort each sub array 
   // grab the last number of each sorted sub array
+  var arrayOfLargestNumbers = []
+  // sort each sub array
   for (var i = 0; i < arr.length; i++) {
     arr[i].sort(function(a,b) {
       return a - b;
     });
-  
-  return arr;
+    var largest = null;
+    for (var sb = 0; sb < arr[i].length; sb++) {
+      largest = arr[i][(arr[i].length -1)];
+    }
+    arrayOfLargestNumbers[i] = largest;
+  }
+  //take last element of each sub array
+  return arrayOfLargestNumbers;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
