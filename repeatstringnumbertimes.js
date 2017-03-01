0b52c04b7021154d1b85e921db5e9003a655eec2
Ian Pan <ianpan@hotmail.com>
2017-03-01 00:27:14 +0000

    repeatstringnumbertimes recursive solution

--- repeatstringnumbertimes.js
+++ repeatstringnumbertimes.js
@@ -9,4 +9,17 @@ function repeatStringNumTimes(str, num) {
   return "";
 }
 
+function repeatStringNumTimes(str, num) {
+  if (num < 0) {
+    return "";
+  } 
+  else if (num === 1) {
+    return str;
+  }
+  else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+
 repeatStringNumTimes("abc", 3);
