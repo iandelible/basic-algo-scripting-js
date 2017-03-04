a33678fe2be03d9e24a05de9e579b93d1f5d7533
Ian Pan <ianpan@hotmail.com>
2017-03-04 03:57:33 +0000

    destroyer.js

---
+++ destroyer.js
@@ -0,0 +1,15 @@
+// return to this problem with Wan
+// The arguments object is an Array-like object corresponding to the arguments passed to a function.
+
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
+}
