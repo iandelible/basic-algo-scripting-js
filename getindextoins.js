fbffb7c08fd8ea257826f6d515130fea4eac60d6
Ian Pan <ianpan@hotmail.com>
2017-03-04 19:49:32 +0000

    getindextoins

---
+++ getindextoins.js
@@ -0,0 +1,13 @@
+// Return the lowest index at which a value (second argument) 
+// should be inserted into an array (first argument) once 
+// it has been sorted. The returned value should be a number.
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort(function (a,b) {
+    return a -b;
+  });
+  return arr.indexOf(num);
+
+}
+
+getIndexToIns([3, 10, 5], 3);
